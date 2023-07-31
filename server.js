8e9180d59a3693f16b7b14dc72ef2be2b4c11039
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-08-01 00:38:09 +0600

    update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 app.use(bodyParser.json())
 const uri = process.env.MONGOBD_URI
@@ -15,6 +17,7 @@ const userSchema = new mongoose.Schema({
     fname: String,
     lname: String,
     email: String,
+    password: String,
     age: Number,
    
 },{
@@ -30,20 +33,45 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to our application' })
     
 })
-let users =[]
-let id = 0
-let getUserbyID = (req) => {
-    const id = req.params.id
-    const user = users.find(u => u.id == id)
-    return user
-}
+
 app.post('/users', async (req, res) => {
-    //res.json({ message: 'Welcome to our application.' })
-    
     try{
-        const user = new User(req.body)
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(req.body.password, salt)
+        const password = hash
+        const userObj = {
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.email,
+            password: password,
+            age: req.body.age
+            
+        }
+        const user = new User(userObj)
         await user.save()
         res.status(201).json(user)
+    }catch(err){
+        console.error(err)
+        res.status(500).json({message: err.message})
+    }
+})
+app.post('/users/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email: email})
+        if(!user){
+            return res.status(404).json({message: 'User not found'})
+        }else{
+            const validPassword = await bcrypt.compare(password, user.password)
+            if(validPassword){
+                const token = jwt.sign({email: user.email, id: user._id},'secret')
+                const userObj = user.toJSON()
+                userObj.accessToken = token
+                res.status(200).json(userObj)
+            }else{
+                return res.status(401).json({message: 'Invalid password'})
+            }
+        }
     }catch(err){
         console.error(err)
         res.status(500).json({message: err.message})
@@ -112,4 +140,3 @@ const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
-// localhost:5000/users 
